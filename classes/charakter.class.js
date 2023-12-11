258cbcc414e44ea63ad5308d436f06539668413b
Alex Solms <duaedin@web.de>
2023-12-11 20:43:43 +0100

    first clouds and chicken

--- classes/charakter.class.js
+++ classes/charakter.class.js
@@ -1,8 +1,8 @@
 class Charakter extends MovableObject{
     x = 50;
-    y = 200;
-    height = 80;
-    width =150;
+    y = 165;
+    height = 200;
+    width = 100;
 
    constructor(){
      super().loadImage('./img/2_character_pepe/1_idle/idle/I-1.png');
--- classes/world.class.js
+++ classes/world.class.js
@@ -1,19 +1,40 @@
-class World{
+class World {
     charakter = new Charakter();
     enemies = [
         new Chicken(),
         new Chicken(),
         new Chicken()
     ];
+    clouds = [
+        new Cloud(),
+        new Cloud(),
+        new Cloud(),
+        new Cloud()
+    ]
+    canvas;
     ctx;
 
-    constructor(canvas){
+    constructor(canvas) {
         this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
         this.draw();
     }
 
 
-    draw(){
-this.ctx.drawImage(this.charakter.img, this.charakter.x, this.charakter.y, this.charakter.height, this.charakter.width);
+    draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.ctx.drawImage(this.charakter.img, this.charakter.x, this.charakter.y, this.charakter.width, this.charakter.height);
+        this.enemies.forEach(enemy => {
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height)
+        })
+        this.clouds.forEach(cloud => {
+            this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height)
+        })
+
+        let self =this;
+        requestAnimationFrame(function(){
+            self.draw();
+        });
     }
 }
