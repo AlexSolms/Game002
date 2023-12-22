664a581743266ed35426099c777fa86e9249ecf4
Alex Solms <duaedin@web.de>
2023-12-22 09:28:21 +0100

    images for the boss added

--- classes/endboss.class.js
+++ classes/endboss.class.js
@@ -18,10 +18,45 @@ class Endboss extends Enemies {
     './img/4_enemie_boss_chicken/1_walk/G3.png',
     './img/4_enemie_boss_chicken/1_walk/G4.png',
   ];
+  attackImages=[
+    './img/4_enemie_boss_chicken/3_attack/G13.png',
+    './img/4_enemie_boss_chicken/3_attack/G17.png',
+    './img/4_enemie_boss_chicken/3_attack/G18.png',
+    './img/4_enemie_boss_chicken/3_attack/G19.png',
+    './img/4_enemie_boss_chicken/3_attack/G20.png'
+  ];
+
+  aleartImages=[
+'./img/4_enemie_boss_chicken/2_alert/G5.png',
+'./img/4_enemie_boss_chicken/2_alert/G6.png',
+'./img/4_enemie_boss_chicken/2_alert/G7.png',
+'./img/4_enemie_boss_chicken/2_alert/G8.png',
+'./img/4_enemie_boss_chicken/2_alert/G9.png',
+'./img/4_enemie_boss_chicken/2_alert/G10.png',
+'./img/4_enemie_boss_chicken/2_alert/G11.png',
+'./img/4_enemie_boss_chicken/2_alert/G12.png'
+  ];
+
+  hurtImages = [
+'./img/4_enemie_boss_chicken/4_hurt/G21.png',
+'./img/4_enemie_boss_chicken/4_hurt/G22.png',
+'./img/4_enemie_boss_chicken/4_hurt/G23.png',
+  ];
+
+  deadImages = [
+    './img/4_enemie_boss_chicken/4_hurt/G23.png',
+    './img/4_enemie_boss_chicken/4_hurt/G24.png',
+    './img/4_enemie_boss_chicken/4_hurt/G25.png',
+  ]
 
   constructor() {
     super().loadImage('./img/4_enemie_boss_chicken/1_walk/G1.png');
     super.loadImages(this.moveImages);
+    super.loadImages(this.attackImages);
+    super.loadImages(this.aleartImages);
+    super.loadImages(this.hurtImages);
+    super.loadImages(this.deadImages);
+
     //super.updateHitbox(0, 0, 40); // x für hitbox
     this.x = 700;
     this.animate();
@@ -31,13 +66,15 @@ class Endboss extends Enemies {
 
     setInterval(() => {
       super.updateHitbox(20, 50, -20);
-      super.playAnimation(this.moveImages);
+      super.playAnimation(this.aleartImages);
+      this.chkCollisionWithbottle();
     }, 130)
 
 
   }
 
-
+// hier muss noch eine Zeitfunktion rein, die die hurt animation nach 1 s abbricht und wieder in die normale animation übergeht
+// bitte einen PAP schreiben!!!!!
   /**
   * this function checks if the chicken was hit by the bottle
   */
@@ -48,7 +85,7 @@ class Endboss extends Enemies {
   }
 
   showEndbossHurt(){
-
+    super.playAnimation(this.hurtImages);
   }
 
 
