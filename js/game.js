8b4f46a91ddcf9937602cadd887ccba5d8606219
Alex Solms <duaedin@web.de>
2024-01-13 22:15:42 +0100

    work on help page

--- js/game.js
+++ js/game.js
@@ -40,29 +40,3 @@ function start(){
 function reStart(){
     window.location.reload();    
 }
-
-
-
-/* window.addEventListener("keydown", (e) => {
-    //console.log(e);
-    if(e.code === "ArrowUp" || e.code === "KeyW") keyboard.up = true;
-    if(e.code === "ArrowDown" || e.code === "KeyS") keyboard.down = true;
-    if(e.code === "ArrowLeft" || e.code === "KeyA") keyboard.left = true;
-    if(e.code === "ArrowRight" || e.code === "KeyD") keyboard.right = true;
-    if( e.code === "KeyE") keyboard.E = true; // bottle throw
-    if(e.code === "KeyB") keyboard.debug = !keyboard.debug; //debug mode
-    if(e.code === "Space") keyboard.space = true;
-    (keyboard.up||keyboard.down||keyboard.left||keyboard.right) ? keyboard.press = true : keyboard.press = false;
- });
- 
- 
- 
- window.addEventListener("keyup", (e) => {
-    if(e.code === "ArrowUp" || e.code === "KeyW") keyboard.up = false;
-    if(e.code === "ArrowDown" || e.code === "KeyS") keyboard.down = false;
-    if(e.code === "ArrowLeft" || e.code === "KeyA") keyboard.left = false;
-    if(e.code === "ArrowRight" || e.code === "KeyD") keyboard.right = false;
-    if( e.code === "KeyE") keyboard.E = false;
-    if(e.code === "Space") keyboard.space = false;
-    keyboard.press = false;
- }); */
